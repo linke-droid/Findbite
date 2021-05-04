80a6fdc136a77e483bf86ca6e033a6aa6e0c2df6
linke-droid <liinus.ingvarsson@gmail.com>
2021-05-04 11:58:55 +0200

    Update app.py
    Co-Authored-By: zeinabbrahmi <81236920+zeinabbrahmi@users.noreply.github.com>
    Co-Authored-By: ghazi9898 <81243435+ghazi9898@users.noreply.github.com>
    Co-Authored-By: simoneskilsson <81237644+simoneskilsson@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import requests
 from flask_sqlalchemy import SQLAlchemy
 from pyodbc import *
 import json
+import requests
+import json
+import api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://OnlineStore:Gst989998@localhost/FindBite?driver=ODBC+Driver+17+for+SQL+Server'
-app.debug=True
+app.debug = True
 db = SQLAlchemy(app)
 
 
@@ -20,70 +23,97 @@ class User(db.Model):
         self.username = username
         self.email = email
         self.password = password
-    
+
     def __repr__(self):
         return '<User %r>' % self.username
 
+
 db.create_all()
-db.session.add(User(username='admin', email='admin@example.com', password='123'))
+db.session.add(
+    User(username='admin', email='admin@example.com', password='123'))
+
 
 @app.route('/result', methods=['GET'])
 def result():
-    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+toronto+canada&key=AIzaSyDBzaptmbm9nuWuFAbjsyxEsz5OGbB0oIs')
+    req = requests.get(
+        'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+toronto+canada&key=AIzaSyDBzaptmbm9nuWuFAbjsyxEsz5OGbB0oIs')
     data = json.loads(req.content)
     return render_template('result.html', data=data)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/api-test', methods=['POST'])
+def demo():
+    protein_food = request.form['Proteinkälla']
+    type_food = request.form['Typ_av_mat']
+    print(type_food)
+    price_food = request.form['price']
+    result = api.get_restaurants(protein_food, type_food, price_food)
+    return render_template('demo.html', restaurants=result)
+
+
 @app.route('/logedin')
 def logedin():
     return render_template('logedin.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/register')
 def register():
     return render_template('register.html')
+
 
 @app.route('/myinfo')
 def myinfo():
     return render_template('myinfo.html')
 
+
 @app.route('/editmyinfo')
 def edit_myinfo():
     return render_template('edit_myinfo.html')
 
+
 @app.route('/post_user', methods=['POST'])
 def post_user():
-        user = User(request.form['username'], request.form['email'], request.form['password'])
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('index'))
+    user = User(request.form['username'],
+                request.form['email'], request.form['password'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 @app.route('/check_login', methods=['GET', 'POST'])
 def log_in():
     if request.method == "POST":
         user_name = request.form['username']
         pass_word = request.form['password']
-        res = User.query.filter_by(username = user_name, password = pass_word).all()
+        res = User.query.filter_by(
+            username=user_name, password=pass_word).all()
         if not res:
             return render_template('wronglogin.html')
         else:
             return render_template('logedin.html')
     else:
         return render_template('index.html')
+
 
 @app.route('/push_new_info', methods=['GET', 'POST'])
 def push_new_info(id):
@@ -97,6 +127,7 @@ def push_new_info(id):
         return redirect(url_for('index'))
     else:
         return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
