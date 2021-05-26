bb90c9bd45c61d118193e9e90e207d6ae1cf08b3
linke-droid <liinus.ingvarsson@gmail.com>
2021-05-26 21:50:39 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
 import requests
 import json
+import glom
+
 # YelpAPI
 # businessId
 
 # key should be hidden
 api_key = 'zJAj4o-JYOnTfrbVIrTeO9HM4S9KaEt71QTyt5Ud-EShBRcH_twkMEjSGTVfJXgHOCVxYEEP92xqS0pQOhhOSIzh2IXZ8bs4Q-bJzp76yMam2xXR4A1FhdOlDJCJYHYx'
 headers = {'Authorization': 'bearer %s' % api_key}
+global protein_food, type_food, price_food
 
 
 def get_restaurants(protein_food, type_food, price_food):
@@ -16,42 +19,32 @@ def get_restaurants(protein_food, type_food, price_food):
         'term': protein_food,
         'categories': type_food,
         'price': price_food,
-        'limit': 1,
-        'location': 'Sweden, Malmö',
-        'sort_by': 'distance'}
+        'limit': 50,
+        'location': 'Sweden, Malmö', }
 
     # request yelp API https://www.yelp.com/developers/documentation/v3/business_search
     response = requests.get(url=search_api_url, headers=headers, params=params)
 
-    business_data = response.json()
-
-    print(business_data.keys())
-
-    for biz in business_data['businesses']:
-        print(biz['name'])
+    businesses = response.json()
+    # loop through businesses list
 
     # convert the data into a json object
     data = json.loads(response.text)
 
     # prints out the json object
-    print(json.dumps(data, indent=4))
+    print(json.dumps(data, indent=4, separators=(". ", " = ")))
 
     # extracts businesses into a python list
     businesses = data['businesses']
 
-    businesses.sort(key=lambda d: d["price"].count('$'))
-
-    # loops over the bussinesses in the list
-    for business in businesses:
-        print('id', business['id'])
-        print('name', business['name'])
-        print('review_count', business['review_count'])
-        print('image_url', business['image_url'])
-        if "price" in business:
-            print('price', business['price'])
-            print('\n')
-    return businesses
-
-
-def sort_price():
-
+    # loops through the list and checks the price range 1-4 and prints it out.
+    for x in range(1, 4):
+        for business in businesses:
+            print('restauarnt', business['name'])
+            print('number of reviews',
+                  business['review_count'])
+            if "price category" in business:
+                if len('price') == x:
+                    print('price', business['price'])
+                    print('\n')
+        return businesses
